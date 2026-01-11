056e0274e144d5a9f17a40c394b6422053e06914
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-11 19:10:20 +0530

    Added testing route for checking the server

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req,res)=>{
+    "Hell00000000.........."
+})
+
 app.listen(PORT, () => {
     connectDB();
     console.log("Server is running on port " , PORT);
