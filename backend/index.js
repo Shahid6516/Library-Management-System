044a339f73e13ac90a8981e0d7dfd6be0ff49d28
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-19 23:41:52 +0530

    Fix import path for database connection in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./Config/db.js";
+import connectDB from "./config/db.js";
 
 const app = express();
 dotenv.config();
