e61251fe9f8a42d1c90ed3db666d3ef1362951e6
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-20 23:40:38 +0530

    Add authentication and controller files for user, book, and transaction management

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+
+export const register = async () => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async () => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
