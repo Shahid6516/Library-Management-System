71d7b3c6326afa791ca8f7a57bf7a3195ca4fb5e
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-21 23:21:14 +0530

    Fix registration response message and implement user login functionality with error handling

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -12,8 +12,8 @@ export const register = async (req, res) => {
     const newUser = new User({ name, email, password: hasedPassword, role });
     await newUser.save();
     res.status(201).json({
-      messgae:`user registered with Name: ${name}`
-    })
+      messgae: `user registered with Name: ${name}`,
+    });
   } catch (error) {
     console.log(error);
   }
@@ -21,6 +21,33 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
+    const { email, password } = req.body;
+    const user = await User.findone({ email });
+    if (!user) {
+      res.status(400).json({
+        message: `User is not existed`,
+      });
+
+      const isMatch = await bcrypt.compare(password, user.passsword);
+      if (!isMatch) {
+        res.status(404).json({
+          message: `invalid credentials`,
+        });
+      }
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      },
+    );
+    res.status(200).json({
+      message: `user logged in successfully`,
+      token,
+    });
+    
   } catch (error) {
     console.log(error);
   }
