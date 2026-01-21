06a962096a97ff7c91eccc38d9b2fa8675640a50
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-21 23:50:11 +0530

    Implement authentication routes and controllers for user registration and login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,17 +1,21 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
 
-    if (name || email) {
-      console.log("user already exist");
+    const exitingUser = await User.findOne({ email });
+    if (exitingUser) {
+      return res.status(400).json({
+        message: `User already exists`,
+      });
     }
     const hasedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ name, email, password: hasedPassword, role });
     await newUser.save();
-    res.status(201).json({
+    return res.status(201).json({
       messgae: `user registered with Name: ${name}`,
     });
   } catch (error) {
@@ -22,18 +26,18 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findone({ email });
+    const user = await User.findOne({ email });
     if (!user) {
-      res.status(400).json({
+      return res.status(400).json({
         message: `User is not existed`,
       });
+    }
 
-      const isMatch = await bcrypt.compare(password, user.passsword);
-      if (!isMatch) {
-        res.status(404).json({
-          message: `invalid credentials`,
-        });
-      }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(404).json({
+        message: `invalid credentials`,
+      });
     }
 
     const token = jwt.sign(
@@ -43,11 +47,10 @@ export const login = async (req, res) => {
         expiresIn: "1d",
       },
     );
-    res.status(200).json({
+    return res.status(200).json({
       message: `user logged in successfully`,
       token,
     });
-    
   } catch (error) {
     console.log(error);
   }
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js"
 
 const app = express();
 dotenv.config();
@@ -11,8 +12,9 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res)=>{
-})
+// route
+
+app.use("/api/auth", authRoute)
 
 
 app.listen(PORT, () => {
