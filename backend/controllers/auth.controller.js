9d1bb701753f086d9637d85142410bb4d7f2cbe9
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-21 23:00:55 +0530

    Implement user registration and login functionality with bcrypt for password hashing

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,25 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
-export const register = async () => {
+export const register = async (req, res) => {
   try {
+    const { name, email, password, role } = req.body;
+
+    if (name || email) {
+      console.log("user already exist");
+    }
+    const hasedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hasedPassword, role });
+    await newUser.save();
+    res.status(201).json({
+      messgae:`user registered with Name: ${name}`
+    })
   } catch (error) {
     console.log(error);
   }
 };
 
-export const login = async () => {
+export const login = async (req, res) => {
   try {
   } catch (error) {
     console.log(error);
