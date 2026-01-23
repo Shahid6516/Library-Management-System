1214aa40a7f870cc18409417d7466a0aa2a889ee
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-23 22:41:53 +0530

    Implement initial structure for bookController with addBook and getAllBooks functions

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,18 @@
+import book from '../models/bookModel.js';
+
+export const addBook = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+export const getAllBooks = async (req, res) => {
+try {
+    
+} catch (error) {
+    
+}
+}
+    
