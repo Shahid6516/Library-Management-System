689cb3c24bf1883b0055491b3ba011723423b09b
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-29 20:16:29 +0530

    Add updateBook and deleteBook functions to bookController for managing book records

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -82,4 +82,46 @@ export const getBookById = async (req, res) => {
   }
 };
 
- 
+ export const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedData = req.body;
+    const updatedBook = await Book.findByIdAndUpdate(id, updatedData, {
+      new: true,
+    });
+    if (!updatedBook) {
+      return res.status(404).json({
+        success: false,
+        message: "Book not found!",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Book updated successfully!",
+      updatedBook,
+    }); 
+  } catch (error) {
+    console.log(error);
+  }
+  };
+
+  export const deleteBook = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedBook = await Book.findByIdAndDelete(id);
+      if (!deletedBook) {
+        return res.status(404).json({
+          success: false,
+          message: "Book not found!",
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        message: "Book deleted successfully!",
+        deletedBook,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
